d205390b6d3ea8c4347780c03a89a4682fa0984d
unknown <tberry6@gmail.com>
2018-03-27 15:43:47 -0700

    Set up files

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+var config = {
+    apiKey: "AIzaSyAAtRXPA9Tlp4TNVsYqks6ys3j04vRIhvI",
+    authDomain: "rps-game-8978e.firebaseapp.com",
+    databaseURL: "https://rps-game-8978e.firebaseio.com",
+    projectId: "rps-game-8978e",
+    storageBucket: "rps-game-8978e.appspot.com",
+    messagingSenderId: "999006540086"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
